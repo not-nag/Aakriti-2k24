76b1028eed156e7cfb1eee018c9198c7801fa822
Nagraj <nagrajtham@gmail..com>
2024-01-03 23:35:15 +0530

    Changes in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div className="min-h-[400px] bg-black"></div>;
+  return (
+    <div>
+      <div className="bg-[#D2F1EE]">
+        <Image
+          src="/first_photo.webp"
+          alt="Aakriti 2K24"
+          width={1920}
+          height={1080}
+          className=""
+        ></Image>
+      </div>
+    </div>
+  );
 }
