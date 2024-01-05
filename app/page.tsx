58d7ffae2caad9301175cb60e3e25d550af5b163
Nagraj <nagrajtham@gmail..com>
2024-01-05 12:23:01 +0530

    Changed homepage

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,10 @@ const roboto_mono = Roboto_Mono({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="bg-black px-2">
+    <div className="bg-black">
       <div className="bg-black h-5"></div>
       <div
-        className={`${roboto_mono.className} bg-black pb-6 pt-2 max-w-[1400px] mx-auto`}
+        className={`${roboto_mono.className} bg-black pb-6 pt-2 max-w-[1400px] mx-auto px-2`}
       >
         <div className="bg-black border-white border-2 rounded-lg sm:text-xl">
           <div className="border-b-2 h-[40px] flex justify-start items-center">
